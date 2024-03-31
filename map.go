2bcabcea78ff35d129143b76d209596a7817c49a
Yash-sudo-web <ymathur123@gmail.com>
2024-03-31 23:24:05 +0530

    added commands

---
+++ map.go
@@ -0,0 +1,9 @@
+package main
+
+func commandMap() error {
+
+}
+
+func commandMapb() error {
+
+}
--- repl.go
+++ repl.go
@@ -7,13 +7,30 @@ import (
 	"strings"
 )
 
-func StartRepl() {
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		scanner := bufio.NewScanner(os.Stdin)
-		print("PokeDex>>")
+		print("Pokédex>> ")
 		scanner.Scan()
-		input := scanner.Text()
-		fmt.Println(input)
+
+		words := textClean(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+		command, ok := getCommands()[commandName]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Invalid command. Type 'help' to see the list of commands.")
+			continue
+		}
+
 	}
 }
 
@@ -21,4 +38,35 @@ func textClean(str string) []string {
 	lowered := strings.ToLower(str)
 	words := strings.Fields(lowered)
 	return words
+}
+
+type commands struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]commands {
+	return map[string]commands{
+		"help": {
+			name:        "help",
+			description: "List all available commands",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit Pokédex",
+			callback:    commandExit,
+		},
+		"map": {
+			name:        "map",
+			description: "Show the next 20 regions",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Show the previous 20 regions",
+			callback:    commandMapb,
+		},
+	}
 }
